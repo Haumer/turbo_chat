e033f6865c38a215b11bf67a633ad2121bdfa2ae
Alexander Haumer <alexander.haumer@me.com>
2026-02-22 21:36:57 +0100

    fix: make invite participant lookup resilient to load order

--- app/controllers/turbo_chat/chats_controller/invitation_support.rb
+++ app/controllers/turbo_chat/chats_controller/invitation_support.rb
@@ -8,7 +8,6 @@ module TurboChat
         participant_class = participant_type.safe_constantize
         return [] if participant_class.nil?
         return [] unless participant_class < ActiveRecord::Base
-        return [] unless participant_class.method_defined?(:active_chat_memberships)
 
         current_member_ids = @chat.chat_memberships.where(removed_at: nil, participant_type: participant_type).pluck(:participant_id)
         participant_class.where.not(id: current_member_ids).order(id: :asc).limit(100).to_a
